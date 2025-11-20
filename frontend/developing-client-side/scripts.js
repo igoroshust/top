e8f178f146540dcc8190aaf2eea9ab5118cbf312
unknown <igoroshust@yandex.ru>
2025-11-20 15:56:32 +0900

    top: bom (window, location, navigator, history)

--- frontend/developing-client-side/scripts.js
+++ frontend/developing-client-side/scripts.js
@@ -1,36 +1,9 @@
-// TODO: попрактиковаться с BOM-элементами
-
-
-
-
-
-function showInfo(){
-    // BOM: информация о браузере
-    alert('User Agent: ' + navigator.userAgent);
-
-    // BOM: текущий URL
-    alert('Текущий URL: ' + location.href);
-
-    // BOM: открыть новое окно
-    window.open('https://composite.website/', '_blank');
-
-    // BOM: история (назад)
-    if (history.length > 1) {
-        history.back();
-    } else {
-        alert('Нет предыдущей страницы в истории!');
-    }
-}
-
-window.addEventListener('resize', () => {
-    console.log('Окно изменено: ' + window.innerWidth + 'x' + window.innerHeight);
+window.addEventListener('popstate', (e) => {
+    console.log('state:', e.state);
 });
 
-// BOM: доступ к DOM через window
-window.onload = function(){
-    // DOM: изменение документа
-    document.body.style.backgroundColor = 'lightblue';
-
-    // BOM: вывод в консоль браузера
-    console.log('Страница загружена. Ширина экрана: ' + screen.width);
-};
+function addHistory(){
+    const state = {page: Date.now()};
+    history.pushState(state, `Page ${state.page}`, `t=${state.page}`);
+    console.log('Добавлена новая страница в историю: ', state);
+}
